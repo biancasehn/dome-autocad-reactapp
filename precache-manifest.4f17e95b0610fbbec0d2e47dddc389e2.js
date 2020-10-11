b768da5f3e8c9f05eb31a2c8862daf5d7a74de74
Bianca Sehn <sehnbiancarafaela135@gmail.com>
2020-10-11 16:16:14 +0200

    Updates

---
+++ precache-manifest.4f17e95b0610fbbec0d2e47dddc389e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8cdcb4c5009b4fac5f31fa127a5553",
+    "url": "/domine-autocad-reactapp/index.html"
+  },
+  {
+    "revision": "9b95cf53b2879c3be7ab",
+    "url": "/domine-autocad-reactapp/static/css/main.e66bc289.chunk.css"
+  },
+  {
+    "revision": "335f13a8f8d39fd58203",
+    "url": "/domine-autocad-reactapp/static/js/2.e683ec3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/domine-autocad-reactapp/static/js/2.e683ec3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b95cf53b2879c3be7ab",
+    "url": "/domine-autocad-reactapp/static/js/main.859e090c.chunk.js"
+  },
+  {
+    "revision": "86be2e541760f54854ce",
+    "url": "/domine-autocad-reactapp/static/js/runtime-main.4084709f.js"
+  }
+]);
